c05b6e88c5445103d5db6b5f75b8a641b9e8da55
kanim <kanim@Kanimuthu.com>
2023-06-14 08:21:03 +0530

    first commit

---
+++ about.php
@@ -0,0 +1,30 @@
+ <!-- Masthead-->
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
+                    	 <h1 class="text-uppercase text-white font-weight-bold">About Us</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+
+    <section class="page-section">
+        <div class="container">
+    <pre> 
+Welcome to our Flight Booking Page!
+
+At our Flight Booking Page, we are dedicated to providing you with a seamless and convenient flight booking experience. Whether you're planning a business trip, a family vacation, or a spontaneous adventure, we have you covered with a wide range of flight options to destinations around the world.
+
+Why Choose Us?
+
+Extensive Flight Options: We understand that every traveler has unique preferences and needs. That's why we offer an extensive selection of flights from numerous airlines, giving you the freedom to choose the perfect itinerary that suits your schedule, budget, and travel requirements. With us, you'll find flights to domestic and international destinations, including popular tourist spots and off-the-beaten-path locations.
+
+Competitive Prices: We believe that booking a flight shouldn't break the bank. Our platform is designed to offer you the best possible prices on flights, ensuring that you get the most value for your money. We regularly update our deals and promotions to provide you with even more savings, making your travel dreams a reality without compromising on quality.
+
+
+
+        </div>
+        </section>
